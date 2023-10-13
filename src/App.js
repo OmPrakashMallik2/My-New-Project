f7907a2c2266f2eab6fafa94662833f642304616
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-10-13 20:48:19 +0530

    good to go now

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="">
+      <h1 className='cursor-not-allowed rounded-md m-5 p-5 bg-gradient-to-r from-black to-red-700
+       text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
     </div>
   );
 }
