27e7364f1fd3eeae12c0e06932612e393000b909
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-10-27 19:19:07 +0530

    set up router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Compo/Login';
 
 function App() {
   return (
-    <div className=" bg-black h-screen p-5">
-      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
-       text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
-      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
-       text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
-      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
-       text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
+    <div className="bg-black h-screen p-5">
+      <h1 className=' p-5 cursor-not-allowed rounded-md bg-gradient-to-r from-gray-800 to-green-600
+       text-blue-50 font-black  text-2xl text-center'>School Management System</h1>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Compo/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1 className='text-red-500 bg-yellow-500'>Log In Page</h1>
+    </div>
+  )
+}
+
+export default Login
