d1b8b67e47b8fa154280e921b6ff8f04535f5e47
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-10-27 09:20:25 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
-      <h1 className='cursor-not-allowed rounded-md m-5 p-5 bg-gradient-to-r from-black to-red-700
+    <div className=" bg-black h-screen p-5">
+      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
        text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
-      <h1 className='cursor-not-allowed rounded-md m-5 p-5 bg-gradient-to-r from-black to-red-700
+      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
        text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
-      <h1 className='cursor-not-allowed rounded-md m-5 p-5 bg-gradient-to-r from-black to-red-700
+      <h1 className='cursor-not-allowed rounded-md bg-gradient-to-r from-black to-red-700
        text-blue-50 font-black  text-2xl text-center'>My New Project</h1>
     </div>
   );
